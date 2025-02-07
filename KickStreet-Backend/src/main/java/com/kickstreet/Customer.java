6e9018d7518dde1b48195700669c3423c64dac5d
Wascar Perez <wascar.perez@yahoo.com>
2025-02-07 18:12:56 -0500

    Add unit test for Customer class.

--- KickStreet-Backend/src/main/java/com/kickstreet/Customer.java
+++ KickStreet-Backend/src/main/java/com/kickstreet/Customer.java
@@ -1,5 +1,33 @@
 package com.kickstreet;
 
+import java.util.UUID;
+
 public class Customer {
     
+    private String name;
+    private String email;
+    private String CustomerId;
+
+    public Customer(String name, String email) {
+
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+
+        if (!email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+
+        this.name = name;
+        this.email = email;
+        this.CustomerId = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- KickStreet-Backend/src/test/java/com/kickstreet/CustomerTest.java
+++ KickStreet-Backend/src/test/java/com/kickstreet/CustomerTest.java
@@ -3,6 +3,44 @@ package com.kickstreet;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Customer Test")
 public class CustomerTest {
-    
+
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer("Emilio Perez", "emilioperez@gmail.com");
+    };
+
+    @Test
+    void testCustomerInit() {
+        Customer customer = new Customer("John Doe", "johndoe@yahoo.com");
+        assertNotNull(customer);
+    };
+
+    @Test
+    void testCustomerInitThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Customer customer = new Customer(null, " ");
+        });
+    };
+
+    @Test
+    void testCustomerGetName() {
+        String customerName = customer.getName();
+        String expectedName = new String("Emilio Perez");
+        assertEquals(expectedName, customerName);
+    }
+
+    @Test
+    void testCustomerGeEmail() {
+        String customerEmail = customer.getEmail();
+        String expectedEmail = new String("emilioperez@gmail.com");
+        assertEquals(expectedEmail, customerEmail);
+    }
+
 }
