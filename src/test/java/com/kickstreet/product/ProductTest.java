0f96460fc09905a6dc5d200f5e76ec6026c1498a
Wascar Perez <wascar.perez@yahoo.com>
2025-02-09 16:40:05 -0500

    Add unit test for Product class.

--- src/test/java/com/kickstreet/product/ProductTest.java
+++ src/test/java/com/kickstreet/product/ProductTest.java
@@ -1,5 +1,9 @@
 package com.kickstreet.product;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
@@ -10,6 +14,23 @@ public class ProductTest {
 
     @BeforeEach
     void setUp() {
-        product = new Product(null, null, 0, 0);
+        product = new Product("AJR3-002", "Air Jordan 3 Retro SE", 200.00, 40);
     };
+
+    @Test
+    void testValidProductInit() {
+        String productId = "AJR1-001";
+        String name = "Air Jordan 1 Retro High";
+        double price = 180.00;
+        int stock = 50;
+
+        Product product = new Product(productId, name, price, stock);
+
+        assertNotNull(product);
+        assertEquals(product.getProductId(), productId);
+        assertEquals(product.getName(), name);
+        assertEquals(product.getPrice(), price);
+        assertEquals(product.getStock(), stock);
+
+    }
 }
